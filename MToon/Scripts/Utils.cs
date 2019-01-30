0662b3825f548658050fcd7fb4ed9548ea5c540d
Masataka SUMI <santarh@gmail.com>
2019-01-30 23:30:27 +0900

    mv enums

--- MToon/Scripts/Utils.cs
+++ MToon/Scripts/Utils.cs
@@ -4,48 +4,6 @@ using UnityEngine.Rendering;
 
 namespace MToon
 {
-    public enum DebugMode
-    {
-        None,
-        Normal,
-        LitShadeRate,
-    }
-
-    public enum OutlineColorMode
-    {
-        FixedColor,
-        MixedLighting
-    }
-
-    public enum OutlineWidthMode
-    {
-        None,
-        WorldCoordinates,
-        ScreenCoordinates
-    }
-
-    public enum RenderMode
-    {
-        Opaque,
-        Cutout,
-        Transparent,
-        TransparentWithZWrite,
-    }
-
-    public enum CullMode
-    {
-        Off,
-        Front,
-        Back,
-    }
-
-    public struct RenderQueueRequirement
-    {
-        public int DefaultValue;
-        public int MinValue;
-        public int MaxValue;
-    }
-
     public static class Utils
     {
         public const string PropDebugMode = "_DebugMode";
