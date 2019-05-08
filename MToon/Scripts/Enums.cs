9b4f0f0d5c2e35189d8bc53105655e01a5366488
Masataka SUMI <santarh@gmail.com>
2019-05-08 18:07:09 +0900

    Add explicit number to enums

--- MToon/Scripts/Enums.cs
+++ MToon/Scripts/Enums.cs
@@ -2,37 +2,37 @@ namespace MToon
 {
     public enum DebugMode
     {
-        None,
-        Normal,
-        LitShadeRate,
+        None = 0,
+        Normal = 1,
+        LitShadeRate = 2,
     }
 
     public enum OutlineColorMode
     {
-        FixedColor,
-        MixedLighting
+        FixedColor = 0,
+        MixedLighting = 1,
     }
 
     public enum OutlineWidthMode
     {
-        None,
-        WorldCoordinates,
-        ScreenCoordinates
+        None = 0,
+        WorldCoordinates = 1,
+        ScreenCoordinates = 2,
     }
 
     public enum RenderMode
     {
-        Opaque,
-        Cutout,
-        Transparent,
-        TransparentWithZWrite,
+        Opaque = 0,
+        Cutout = 1,
+        Transparent = 2,
+        TransparentWithZWrite = 3,
     }
 
     public enum CullMode
     {
-        Off,
-        Front,
-        Back,
+        Off = 0,
+        Front = 1,
+        Back = 2,
     }
 
     public struct RenderQueueRequirement
